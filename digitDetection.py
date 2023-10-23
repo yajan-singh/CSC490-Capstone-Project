f4e92c83d243f7d55c5e66b8c85a43d6f5d2bfe4
Vivek Patel <70912177+Viv3k-P@users.noreply.github.com>
2023-10-23 16:16:55 -0400

    Update digitDetection.py

--- digitDetection.py
+++ digitDetection.py
@@ -1,32 +1,46 @@
-import cv2
 import numpy as np
-import os
+from skimage.transform import resize
+from skimage import measure
+from skimage.measure import regionprops
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import DetectPlate
 
-# Load MNIST dataset
-MNIST = tf.keras.datasets.mnist
+# The invert was done so as to convert the black pixel to white pixel and vice versa
+license_plate = np.invert(DetectPlate.plate_like_objects[0])
 
-# Split dataset into training and testing sets
-(x_train, y_train), (x_test, y_test) = MNIST.load_data()
+labelled_plate = measure.label(license_plate)
 
-# Normalize the data
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+fig, ax1 = plt.subplots(1)
+ax1.imshow(license_plate, cmap="gray")
+# the next two lines is based on the assumptions that the width of
+# a license plate should be between 5% and 15% of the license plate,
+# and height should be between 35% and 60%
+# this will eliminate some
+character_dimensions = (0.35*license_plate.shape[0], 0.60*license_plate.shape[0], 0.05*license_plate.shape[1], 0.15*license_plate.shape[1])
+min_height, max_height, min_width, max_width = character_dimensions
 
-# Create the model
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+characters = []
+counter=0
+column_list = []
+for regions in regionprops(labelled_plate):
+    y0, x0, y1, x1 = regions.bbox
+    region_height = y1 - y0
+    region_width = x1 - x0
 
-# Compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    if region_height > min_height and region_height < max_height and region_width > min_width and region_width < max_width:
+        roi = license_plate[y0:y1, x0:x1]
 
-# Train the model
-model.fit(x_train, y_train, epochs=3)
+        # draw a red bordered rectangle over the character.
+        rect_border = patches.Rectangle((x0, y0), x1 - x0, y1 - y0, edgecolor="red",
+                                       linewidth=2, fill=False)
+        ax1.add_patch(rect_border)
 
-# Save the model
-model.save('trainedNumber.model')
+        # resize the characters to 20X20 and then append each character into the characters list
+        resized_char = resize(roi, (20, 20))
+        characters.append(resized_char)
+
+        # this is just to keep track of the arrangement of the characters
+        column_list.append(x0)
+# print(characters)
+plt.show()
